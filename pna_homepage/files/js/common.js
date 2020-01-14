7420933d6549e91d50f022d9941538410e1cc1d0
hanesuu <hanesu.su@gmail.com>
2020-01-14 17:25:50 +0900

    pna homepage upload

---
+++ pna_homepage/files/js/common.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+//m-gnb
+$(".btn-m-gnb").click(function(){
+  $(this).next("nav").animate({right:"0"},350);
+  $(this).siblings(".bg-dim").addClass("on");
+});
+$(".btn-m-close, .bg-dim").click(function(){
+  $(".m-gnb nav").animate({right:"-200px"},350);
+  $(".bg-dim").removeClass("on");
+});
+
+//selectbox
+$(function() {
+	var selectTarget = $('.select select');
+	// focus 가 되었을 때와 focus 를 잃었을 때
+	selectTarget.on({
+		'focus': function() {
+			$(this).parent().addClass('focus');
+		},
+		'blur': function() {
+		  $(this).parent().removeClass('focus');
+		}
+	});
+
+	selectTarget.change(function(){
+    var select_name = $(this).children('option:selected').text();
+    $(this).siblings('label').text(select_name);
+	});
+});
+
+//scroll-top
+$(window).scroll(function(){
+  if($(this).scrollTop() > 200){
+    $(".top").fadeIn();
+  } else {
+    $(".top").fadeOut();
+  }
+});
+$(".top").click(function(){
+  $('html, body').animate({scrollTop:0},400);
+  return false;
+});
+
+//이용가능API 매뉴얼보기 포커스
+$(".pg-api").find(".btn-sm").hover(function(){
+	$(this).parent().toggleClass('hover');
+});
+
+//돈버는법 간편설정끝 클래스
+$(window).scroll(function(){
+  if($(this).scrollTop() > 700){
+    $(".sect-finish").addClass('active');
+  } else {
+    $(".sect-finish").removeClass('active');
+  }
+});
+
+//탭메뉴
+$(".tab_content").hide();
+$(".tab_content:first").show();
+
+$(".wrap-tab li").click(function () {
+    $(".wrap-tab li").removeClass("active");
+    $(this).addClass("active");
+    $(".tab_content").hide()
+    var activeTab = $(this).attr("rel");
+    $("#" + activeTab).show()
+});
+
+//collapse
+$(".con-collapse").hide();
+$(".tit-collapse").click(function(){
+  $(this).parent().toggleClass("active").children(".con-collapse").slideToggle(200);
+});
+
+
+});
+
+//레이어팝업
+function fnShowPop(sGetName){
+    var $layer = $("#"+ sGetName);
+    $layer.addClass("on");
+}
+function fnHidePop(sGetName){
+    $("#"+ sGetName).removeClass("on");
+}
