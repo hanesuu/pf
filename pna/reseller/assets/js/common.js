5f50121a8617d2a6f72a5c271554122a83ec185f
hanesuu <hanesu.su@gmail.com>
2020-01-16 09:39:32 +0900

    공용 파일 생성 및 연결

--- pna/reseller/assets/js/common.js
+++ pna/reseller/assets/js/common.js
@@ -17,22 +17,4 @@ $(document).ready(function(){
 	$(".depth1.add-arr").children("a").click(function(){
 		$(this).parent(".add-arr").toggleClass("active").children(".depth2").slideToggle(200);
 	});
-
-	/* 게시판 오름차 내림차 */
-	$(".board-list").find(".sort").toggle(function(){
-		$(this).siblings().removeClass("dsc","asc");
-		$(this).removeClass("asc").addClass("dsc");
-	},function(){
-		$(this).removeClass("dsc").addClass("asc");
-	});
-	
-	/* 미리보기용 값들 */
-	$(".wrap-chart-list").find(".edit").hide();
-	$(".wrap-chart-list").children(".tbl-filter.index").find(".btn-line-b").click(function(){
-		$(this).parent().parent(".index").hide().next(".edit").css("display","flex").next(".index").hide().next(".edit").show();
-	});
-	$(".wrap-chart-list").children(".tbl-filter.edit").find(".btn-line-b").click(function(){
-		$(this).parent().parents(".edit").hide().prev(".index").css("display","flex").next().next(".index").show().next(".edit").hide();
-	});
-
 });
---
+++ pna/reseller/assets/js/page.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	/* 게시판 오름차 내림차 */
+	$(".board-list").find(".sort").toggle(function(){
+		$(this).siblings().removeClass("dsc","asc");
+		$(this).removeClass("asc").addClass("dsc");
+	},function(){
+		$(this).removeClass("dsc").addClass("asc");
+	});
+	
+	/* 미리보기용 값들 */
+	$(".wrap-chart-list").find(".edit").hide();
+	$(".wrap-chart-list").children(".tbl-filter.index").find(".btn-line-b").click(function(){
+		$(this).parent().parent(".index").hide().next(".edit").css("display","flex").next(".index").hide().next(".edit").show();
+	});
+	$(".wrap-chart-list").children(".tbl-filter.edit").find(".btn-line-b").click(function(){
+		$(this).parent().parents(".edit").hide().prev(".index").css("display","flex").next().next(".index").show().next(".edit").hide();
+	});
+
+});
