14ab4223e29eaceec96df050c19e7dcb61de2cef
hanesuu <hanesu.su@gmail.com>
2020-01-17 17:29:46 +0900

    js파일 분리 적용

---
+++ pna/homepage/files/js/gnb.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+//gnb
+ $(".h-index").children("#header").find("li:nth-child(1)").addClass("active");
+ $(".h-api").children("#header").find("li:nth-child(2)").addClass("active");
+ $(".h-sell").children("#header").find("li:nth-child(3)").addClass("active");
+ $(".h-calc").children("#header").find("li:nth-child(4)").addClass("active");
+ $(".h-help").children("#header").find("li:nth-child(5)").addClass("active");
+
+});
