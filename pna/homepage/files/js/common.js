ee98517e77aea4f509ad3066d1477fe37deff09d
hanesuu <hanesu.su@gmail.com>
2020-01-17 14:19:29 +0900

    홈페이지 > 메뉴 액티브 테스트

--- pna/homepage/files/js/common.js
+++ pna/homepage/files/js/common.js
@@ -73,6 +73,13 @@ $(".tit-collapse").click(function(){
   $(this).parent().toggleClass("active").children(".con-collapse").slideToggle(200);
 });
 
+//gnb
+ $(".h-index").children("#header").find("li:nth-child(1)").addClass("active");
+ $(".h-api").children("#header").find("li:nth-child(2)").addClass("active");
+ $(".h-sell").children("#header").find("li:nth-child(3)").addClass("active");
+ $(".h-calc").children("#header").find("li:nth-child(4)").addClass("active");
+ $(".h-help").children("#header").find("li:nth-child(5)").addClass("active");
+
 
 });
 
