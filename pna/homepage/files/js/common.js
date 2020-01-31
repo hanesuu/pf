4dc215f7879322df2289ae39f13fdc154e3fd963
hanesuu <hanesu.su@gmail.com>
2020-01-31 11:06:00 +0900

    홈페이지 > gnb active test

--- pna/homepage/files/js/common.js
+++ pna/homepage/files/js/common.js
@@ -1,15 +1,5 @@
 $(document).ready(function(){
 
-//gnb
-$(init);
-function init() {
-  var menuSeq = parseInt('${param.menuSeq}');
-  if(!menuSeq) {
-    menuSeq = 1;
-  }
-  $('nav li').eq(menuSeq - 1).addClass('active');
-}
-
 //m-gnb
 $(".btn-m-gnb").click(function(){
   $(this).next("nav").animate({right:"0"},350);
