b3222f2c105be847a678937b43ddf8635a739185
hanesuu <hanesu.su@gmail.com>
2020-04-21 14:45:12 +0900

    egg admin > gnb 스타일 조정, 대시보드 그래프 설정값 추가

--- egg/admin/files/js/common.js
+++ egg/admin/files/js/common.js
@@ -4,13 +4,12 @@ $(".page-logo").children("button").click(function(){
 	$(this).parent().toggleClass("less");
 	$(".gnb").toggleClass("less");
 	$(".page-content").toggleClass("less");
+	$(".nav-item").removeClass("open");
 });
 
 //gnb toggle
-$(".sub-menu").hide();
 $(".gnb").children().children(".nav-item").click(function(){
 	$(this).toggleClass("open");
-	$(this).children(".sub-menu").slideToggle(200);
 });
 //mobile gnb
 $("header").children(".btn-m-gnb").click(function(){
