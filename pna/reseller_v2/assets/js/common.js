e75a5831859c7ae0816dbf538dae548505b29acb
hanesuu <hanesu.su@gmail.com>
2020-07-22 15:25:08 +0900

    pna reseller v2 > header shadow 삭제, 리셀러리스트 검색바 fixed

--- pna/reseller_v2/assets/js/common.js
+++ pna/reseller_v2/assets/js/common.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
 	/* gnb layout */
 	$(".header").find(".btn-nav").click(function(){
-	  $(this).parents(".top").next("nav").toggleClass("hide");
-	  $(this).parents("#app").find(".wrap-content").toggleClass("wide");
-	  $(this).parents("#app").find(".footer").toggleClass("wide");
+		$(this).parents(".top").next("nav").toggleClass("hide");
+		$(this).parents("#app").find(".wrap-content").toggleClass("wide");
+		$(this).parents("#app").find(".footer").toggleClass("wide");
+		$(this).parents("#app").find(".topbar").toggleClass("wide");
 	});	
 	$(window).resize(function (){		
 		if ($("body").width() < 768 ){
 			$(".header").children("nav").addClass("hide");
 			$("#app").find(".wrap-content").addClass("wide");
 			$("#app").find(".footer").addClass("wide");
+			$("#app").find(".topbar").addClass("wide");
 		}
 	});
 
@@ -44,14 +46,14 @@ $(document).ready(function(){
 	$(".mjstore-2").find(".top").children(".location").children(".menu2").text("판매점 상세");
 
 	//결제 관리
-    $(".mj3").find(".nav > ul > li:nth-child(4)").addClass("active");
-    $(".mj3").find(".top").children(".location").children(".menu1").text("결제 관리");
-    $(".mj3").find(".top").children(".location").children(".menu2").text("결제 현황");
+	$(".mj3").find(".nav > ul > li:nth-child(4)").addClass("active");
+	$(".mj3").find(".top").children(".location").children(".menu1").text("결제 관리");
+	$(".mj3").find(".top").children(".location").children(".menu2").text("결제 현황");
 
 	//유료 API 관리
-    $(".mjapi").find(".nav > ul > li:nth-child(5)").addClass("active");
-    $(".mjapi").find(".top").children(".location").children(".menu1").text("유료 API 관리");
-    $(".mjapi").find(".top").children(".location").children(".menu2").text("유료 API 현황");
+	$(".mjapi").find(".nav > ul > li:nth-child(5)").addClass("active");
+	$(".mjapi").find(".top").children(".location").children(".menu1").text("유료 API 관리");
+	$(".mjapi").find(".top").children(".location").children(".menu2").text("유료 API 현황");
 
 	//정산 관리	
 	$(".mjadjust").find(".nav > ul > li:nth-child(6)").addClass("active").children(".depth2").show().children("li:first-child").addClass("active");
@@ -62,25 +64,25 @@ $(document).ready(function(){
 	$(".mjadjust-1").find(".top").children(".location").children(".menu2").text("API 마진 정산");
 
     //설정
-	$(".mj7-1").find(".nav > ul > li:nth-child(7)").addClass("active").children(".depth2").show().children("li:first-child").addClass("active");
-	$(".mj7-1").find(".top").children(".location").children(".menu1").text("설정");
+    $(".mj7-1").find(".nav > ul > li:nth-child(7)").addClass("active").children(".depth2").show().children("li:first-child").addClass("active");
+    $(".mj7-1").find(".top").children(".location").children(".menu1").text("설정");
     $(".mj7-1").find(".top").children(".location").children(".menu2").text("계정정보");
-	$(".mj7-2").find(".nav > ul > li:nth-child(7)").addClass("active").children(".depth2").show().children("li:nth-child(2)").addClass("active");
+    $(".mj7-2").find(".nav > ul > li:nth-child(7)").addClass("active").children(".depth2").show().children("li:nth-child(2)").addClass("active");
     $(".mj7-2").find(".top").children(".location").children(".menu1").text("설정");
     $(".mj7-2").find(".top").children(".location").children(".menu2").text("비밀번호 변경");
 
     //고객센터
-	$(".mj6-1").find(".nav > ul > li:nth-child(8)").addClass("active").children(".depth2").show().children("li:first-child").addClass("active");
-	$(".mj6-1").find(".top").children(".location").children(".menu1").text("고객센터");
+    $(".mj6-1").find(".nav > ul > li:nth-child(8)").addClass("active").children(".depth2").show().children("li:first-child").addClass("active");
+    $(".mj6-1").find(".top").children(".location").children(".menu1").text("고객센터");
     $(".mj6-1").find(".top").children(".location").children(".menu2").text("공지사항");
-	$(".mj6-2").find(".nav > ul > li:nth-child(8)").addClass("active").children(".depth2").show().children("li:nth-child(2)").addClass("active");
+    $(".mj6-2").find(".nav > ul > li:nth-child(8)").addClass("active").children(".depth2").show().children("li:nth-child(2)").addClass("active");
     $(".mj6-2").find(".top").children(".location").children(".menu1").text("고객센터");
     $(".mj6-2").find(".top").children(".location").children(".menu2").text("자주 묻는 질문");
-	$(".mj6-3").find(".nav > ul > li:nth-child(8)").addClass("active").children(".depth2").show().children("li:nth-child(3)").addClass("active");
+    $(".mj6-3").find(".nav > ul > li:nth-child(8)").addClass("active").children(".depth2").show().children("li:nth-child(3)").addClass("active");
     $(".mj6-3").find(".top").children(".location").children(".menu1").text("고객센터");
     $(".mj6-3").find(".top").children(".location").children(".menu2").text("문의내역");
 
-	$(".mj6-4").find(".nav > ul > li:nth-child(8)").addClass("active").children(".depth2").show().children("li:nth-child(4)").addClass("active");
+    $(".mj6-4").find(".nav > ul > li:nth-child(8)").addClass("active").children(".depth2").show().children("li:nth-child(4)").addClass("active");
     $(".mj6-4").find(".top").children(".location").children(".menu1").text("고객센터");
     $(".mj6-4").find(".top").children(".location").children(".menu2").text("문의하기");
 
@@ -91,11 +93,11 @@ $(document).ready(function(){
             //input태그의 name이 chk인 태그들을 찾아서 checked옵션을 true로 정의
             $("input[name=chk]").prop("checked",true);
             //클릭이 안되있으면
-        }else{
+          }else{
             //input태그의 name이 chk인 태그들을 찾아서 checked옵션을 false로 정의
             $("input[name=chk]").prop("checked",false);
-        }
-    });
+          }
+        });
 
     /* 판매점 목록 sticky layout */
     $(".bx-right").children(".bx-basic").hide();
@@ -111,4 +113,4 @@ $(document).ready(function(){
     	$(this).parents(".board").toggleClass("active");
     	$(".bx-right").children("#bxAdjust").show();
     });
-});
+  });
