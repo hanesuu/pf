a4a3bafcb2f50e5474e5076d84fed38e18f7231e
hanesuu <hanesu.su@gmail.com>
2021-05-31 13:21:02 +0900

    me > include test

--- me/assets/js/script.js
+++ me/assets/js/script.js
@@ -11,3 +11,22 @@ $(document).ready(function(){
 	});
 
 });
+
+
+// include
+ window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+}); 
